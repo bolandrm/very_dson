0dc8ca49bf7440a73f22aff0af728e6c06289d5f
ishakir <shakimra@uk.ibm.com>
2014-06-28 12:22:19 +0100

    Make sure we can request html and dson independantly

--- test/dummy/app/controllers/doge_controller.rb
+++ test/dummy/app/controllers/doge_controller.rb
@@ -6,6 +6,13 @@ class DogeController < ApplicationController
     end
   end
 
+  def choice
+    respond_to do |f|
+      f.html
+      f.dson { render_it }
+    end
+  end
+
   def render_it
     render dson: { "foo" => ["bar", "baz", "fizzbuzz"], doge: true }
   end
